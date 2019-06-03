cbaeea4dbdcb86bfbefb8f85f8812accfdb8f052
amy null <null@amy.gg>
2019-06-03 02:57:14 -0400

    Bump Elixir version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Raindrop.Mixfile do
     [
       app: :raindrop,
       version: "0.0.1",
-      elixir: "~> 1.4",
+      elixir: "~> 1.8",
       elixirc_paths: elixirc_paths(Mix.env),
       compilers: [:phoenix, :gettext] ++ Mix.compilers,
       start_permanent: Mix.env == :prod,
